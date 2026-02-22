4b5ee5dfb3ae121441d32aed2b7198667d3a4114
SOFTAPP TECHNOLOGIES <janakpurisoftapp@gmail.com>
2026-02-22 12:12:32 +0000

    Add new file

---
+++ terraform/master.tf
@@ -0,0 +1,135 @@
+terraform {
+
+  required_providers {
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23"
+    }
+
+  }
+
+}
+
+provider "kubernetes" {
+
+  config_path = "~/.kube/config"
+
+}
+
+# ============================
+# Create Namespace
+# ============================
+
+resource "kubernetes_namespace" "devops_ns" {
+
+  metadata {
+
+    name = "devops-sonarqube"
+
+  }
+
+}
+
+# ============================
+# Create Deployment
+# ============================
+
+resource "kubernetes_deployment" "devops_app" {
+
+  metadata {
+
+    name      = "devops-sonarqube-app"
+    namespace = kubernetes_namespace.devops_ns.metadata[0].name
+
+    labels = {
+
+      app = "devops-sonarqube"
+
+    }
+
+  }
+
+  spec {
+
+    replicas = 2
+
+    selector {
+
+      match_labels = {
+
+        app = "devops-sonarqube"
+
+      }
+
+    }
+
+    template {
+
+      metadata {
+
+        labels = {
+
+          app = "devops-sonarqube"
+
+        }
+
+      }
+
+      spec {
+
+        container {
+
+          name  = "devops-container"
+          image = "shivsoftapp/devops-sonarqube-image:33"
+
+          port {
+
+            container_port = 5000
+
+          }
+
+        }
+
+      }
+
+    }
+
+  }
+
+}
+
+# ============================
+# Create Service
+# ============================
+
+resource "kubernetes_service" "devops_service" {
+
+  metadata {
+
+    name      = "devops-sonarqube-service"
+    namespace = kubernetes_namespace.devops_ns.metadata[0].name
+
+  }
+
+  spec {
+
+    selector = {
+
+      app = "devops-sonarqube"
+
+    }
+
+    port {
+
+      port        = 5000
+      target_port = 5000
+      node_port   = 30007
+
+    }
+
+    type = "NodePort"
+
+  }
+
+}
